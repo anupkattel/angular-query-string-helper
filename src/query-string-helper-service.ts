ce789718d05138bb45d15985f7d70cd0952b2a65
Anup Kattel <anup.kattel@4mation.com>
2017-12-05 16:52:28 +1100

    Fixed the space issue in the query string values where + wasn't getting converted back to space.

--- src/query-string-helper-service.ts
+++ src/query-string-helper-service.ts
@@ -4,10 +4,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class QueryStringHelperService {
 
-    private GetQueryString(): { [s: string]: string; } {
+    get QueryString(): { [s: string]: string; } {
         var search = location.search.substring(1)
-        return search ?
-            JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-            : {}
-    }    
+        if (search) {
+            return JSON.parse('{"' +
+                decodeURI(search.replace(/\+/g, '%20')) //Replace the + with spaces before decoding to preserve actual + sign
+                    .replace(/"/g, '\\"')   // unescape the double quotes
+                    .replace(/&/g, '","')   // & separates the key value pairs so create a new property by using a comma
+                    .replace(/=/g, '":"')   // = should be replaced to : for json objects.
+                + '"}')
+        }
+
+        return {}
+    }
 }
